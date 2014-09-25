5665f5a655a89bc4008e65ae721f515d6404ee1a
tilir <konstantin.vladimirov@gmail.com>
2014-09-25 13:39:06 +0000

    Errata to arrays&pointers and extern C explanation
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@30 39143b06-f351-456c-9f92-4cd32fad6823

--- cpp_code/p1-pointers.cpp
+++ cpp_code/p1-pointers.cpp
@@ -1,3 +1,5 @@
+typedef int unsized_arr_t[];
+
 int 
 foo (int x)
 {
@@ -15,8 +17,43 @@ int
 baz (int x)
 {
   int y[10] = {4, x};
+  int z [][1] = {{1}, {2}};
   int *p = y;
   int t = *(y + 3);
   return p[t];
 }
 
+int
+err (int x[], int *y)
+{
+  int a = 5;
+  y = x;
+  x = &a;
+  return *x;
+}
+
+int
+callee (int x[16])
+{
+  return x[15] + x[0];
+}
+
+int
+caller (int *x)
+{
+  return callee (x);
+}
+
+int 
+caller2 (unsized_arr_t x)
+{
+  return callee (x);
+}
+
+#if 0
+unsized_arr_t
+caller3 (int x)
+{
+  return &x;
+}
+#endif
