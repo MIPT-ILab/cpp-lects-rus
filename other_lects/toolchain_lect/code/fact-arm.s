b8b4795af97243ae529a07cc473ccb1489ec9b98
tilir <konstantin.vladimirov@gmail.com>
2016-02-28 22:16:07 +0000

    Resolving TODOs in alpha-release toolchain lect, adding ARM example
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@100 39143b06-f351-456c-9f92-4cd32fad6823

---
+++ other_lects/toolchain_lect/code/fact-arm.s
@@ -0,0 +1,201 @@
+	.cpu arm7tdmi
+	.fpu softvfp
+	.eabi_attribute 20, 1
+	.eabi_attribute 21, 1
+	.eabi_attribute 23, 3
+	.eabi_attribute 24, 1
+	.eabi_attribute 25, 1
+	.eabi_attribute 26, 1
+	.eabi_attribute 30, 6
+	.eabi_attribute 34, 0
+	.eabi_attribute 18, 4
+	.file	"fact.c"
+	.text
+.Ltext0:
+	.cfi_sections	.debug_frame
+	.align	2
+	.global	fact
+	.type	fact, %function
+fact:
+.LFB0:
+	.file 1 "fact.c"
+	.loc 1 3 0
+	.cfi_startproc
+	@ Function supports interworking.
+	@ args = 0, pretend = 0, frame = 8
+	@ frame_needed = 1, uses_anonymous_args = 0
+	stmfd	sp!, {fp, lr}
+	.cfi_def_cfa_offset 8
+	.cfi_offset 11, -8
+	.cfi_offset 14, -4
+	add	fp, sp, #4
+	.cfi_def_cfa 11, 4
+	sub	sp, sp, #8
+	str	r0, [fp, #-8]
+	.loc 1 4 0
+	ldr	r3, [fp, #-8]
+	cmp	r3, #1
+	bhi	.L2
+	.loc 1 5 0
+	mov	r3, #1
+	b	.L3
+.L2:
+	.loc 1 7 0
+	ldr	r3, [fp, #-8]
+	sub	r3, r3, #1
+	mov	r0, r3
+	bl	fact
+	mov	r3, r0
+	ldr	r2, [fp, #-8]
+	mul	r3, r2, r3
+.L3:
+	.loc 1 8 0
+	mov	r0, r3
+	sub	sp, fp, #4
+	@ sp needed
+	ldmfd	sp!, {fp, lr}
+	bx	lr
+	.cfi_endproc
+.LFE0:
+	.size	fact, .-fact
+.Letext0:
+	.section	.debug_info,"",%progbits
+.Ldebug_info0:
+	.4byte	0x4f
+	.2byte	0x4
+	.4byte	.Ldebug_abbrev0
+	.byte	0x4
+	.uleb128 0x1
+	.4byte	.LASF0
+	.byte	0x1
+	.4byte	.LASF1
+	.4byte	.LASF2
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	.Ldebug_line0
+	.uleb128 0x2
+	.4byte	.LASF3
+	.byte	0x1
+	.byte	0x2
+	.4byte	0x4b
+	.4byte	.LFB0
+	.4byte	.LFE0-.LFB0
+	.uleb128 0x1
+	.byte	0x9c
+	.4byte	0x4b
+	.uleb128 0x3
+	.ascii	"x\000"
+	.byte	0x1
+	.byte	0x2
+	.4byte	0x4b
+	.uleb128 0x2
+	.byte	0x91
+	.sleb128 -12
+	.byte	0
+	.uleb128 0x4
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF4
+	.byte	0
+	.section	.debug_abbrev,"",%progbits
+.Ldebug_abbrev0:
+	.uleb128 0x1
+	.uleb128 0x11
+	.byte	0x1
+	.uleb128 0x25
+	.uleb128 0xe
+	.uleb128 0x13
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x1b
+	.uleb128 0xe
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x6
+	.uleb128 0x10
+	.uleb128 0x17
+	.byte	0
+	.byte	0
+	.uleb128 0x2
+	.uleb128 0x2e
+	.byte	0x1
+	.uleb128 0x3f
+	.uleb128 0x19
+	.uleb128 0x3
+	.uleb128 0xe
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x27
+	.uleb128 0x19
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x11
+	.uleb128 0x1
+	.uleb128 0x12
+	.uleb128 0x6
+	.uleb128 0x40
+	.uleb128 0x18
+	.uleb128 0x2116
+	.uleb128 0x19
+	.uleb128 0x1
+	.uleb128 0x13
+	.byte	0
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x5
+	.byte	0
+	.uleb128 0x3
+	.uleb128 0x8
+	.uleb128 0x3a
+	.uleb128 0xb
+	.uleb128 0x3b
+	.uleb128 0xb
+	.uleb128 0x49
+	.uleb128 0x13
+	.uleb128 0x2
+	.uleb128 0x18
+	.byte	0
+	.byte	0
+	.uleb128 0x4
+	.uleb128 0x24
+	.byte	0
+	.uleb128 0xb
+	.uleb128 0xb
+	.uleb128 0x3e
+	.uleb128 0xb
+	.uleb128 0x3
+	.uleb128 0xe
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",%progbits
+	.4byte	0x1c
+	.2byte	0x2
+	.4byte	.Ldebug_info0
+	.byte	0x4
+	.byte	0
+	.2byte	0
+	.2byte	0
+	.4byte	.Ltext0
+	.4byte	.Letext0-.Ltext0
+	.4byte	0
+	.4byte	0
+	.section	.debug_line,"",%progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",%progbits,1
+.LASF0:
+	.ascii	"GNU C 4.8.2 -g -O0\000"
+.LASF4:
+	.ascii	"unsigned int\000"
+.LASF2:
+	.ascii	"/home/tilir/Projects/writing stuff/cpp_lectures/sou"
+	.ascii	"rceforge/other_lects/toolchain_lect/code\000"
+.LASF3:
+	.ascii	"fact\000"
+.LASF1:
+	.ascii	"fact.c\000"
+	.ident	"GCC: (4.8.2-14ubuntu1+6) 4.8.2"
