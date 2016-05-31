fc2854c60003ee570de255c854aa926f97743c76
tilir <konstantin.vladimirov@gmail.com>
2016-05-31 09:06:39 +0000

    Last moment fixes
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@139 39143b06-f351-456c-9f92-4cd32fad6823

--- cpp_code/concepts/checkc.cc
+++ cpp_code/concepts/checkc.cc
@@ -1,6 +1,8 @@
 #include <utility>
 #include <iostream>
 
+#if 0
+
 template <typename C>
 concept bool Int = std::is_integral<C>::value;
 
@@ -8,14 +10,43 @@ void check (Int x) {
   std::cout << "Brief" << std::endl;
 }
 
+constexpr bool check_always () { return true; }
+
 template <typename C> 
-requires std::is_integral<C>::value
+requires /* std::is_integral<C>::value ||*/ check_always()
 void check (C x) {
   std::cout << "Full" << std::endl;
 }
 
+#endif
+
+template<typename T>
+concept bool Subsumed() {
+    return requires () { typename T::type1; };
+}
+
+template<typename T>
+concept bool Subsuming() {
+    return Subsumed<T>()
+        && requires () { typename T::type2; };
+}
+
+struct M {
+    using type1 = int;
+    using type2 = int;
+};
+
+template<typename T>
+struct TM;
+
+template<Subsumed T>
+struct TM<T> { TM() {std::cout << "Subsumed!\n";}  };
+
+template<Subsuming T>
+struct TM<T> { TM() {std::cout << "Subsuming!\n";}  };
+
 int 
 main (void) {
-  check<Int> (1);
+  TM<M> X{};
   return 0;
 }
