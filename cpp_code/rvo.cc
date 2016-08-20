c6fdf1d4adf5a76c49e87a96f88e1dee8a3fdaa1
tilir <konstantin.vladimirov@gmail.com>
2016-08-20 18:38:20 +0000

    Some files to examples
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@147 39143b06-f351-456c-9f92-4cd32fad6823

---
+++ cpp_code/rvo.cc
@@ -0,0 +1,25 @@
+#include <cstdio>
+
+using namespace std;
+
+class foo {
+public:
+  foo () { printf ("foo::foo()\n"); }
+  foo (const foo&) { printf ("foo::foo( const foo& )\n"); }
+  ~foo () { printf ("foo::~foo()\n"); }
+};
+
+foo
+bar()
+{
+  foo local_foo;
+  return local_foo;
+}
+
+int
+main()
+{
+  foo f = bar();
+  return 0;
+}
+
