8d8747164bc99c5168f79c0e9e835b84e862d54c
tilir <konstantin.vladimirov@gmail.com>
2017-02-23 16:39:25 +0000

    Adding slides to repo for nearest lectures
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@171 39143b06-f351-456c-9f92-4cd32fad6823

---
+++ cpp_code/iostreams/01-setbuf.cc
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <ctime>
+
+using std::fprintf;
+
+void delay(int seconds)
+{
+	time_t start, current;
+	time(&start);
+	do { time(&current); } while ((current - start) < seconds);
+}
+
+int main ()
+{
+	// buffered IO
+	// setvbuf (stdout, NULL, _IOFBF, 1024);
+	
+	// unbuffered IO
+	// setbuf (stdout, NULL);
+	
+	fprintf (stdout, "%s, ", "Hello");
+	delay(5);
+	fprintf (stdout, "%s!\n", "world");
+}
+
---
+++ cpp_code/iostreams/02-fmtspec.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main (void)
+{
+	int n = 42;
+	cout << n << endl;
+	cout << std::hex << n << endl;
+	cout << std::oct << n << endl;
+	return 0;
+}
