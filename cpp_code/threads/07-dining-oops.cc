d609f5138687f1b369ba22c6b450e69bb2963c4c
tilir <konstantin.vladimirov@gmail.com>
2019-02-07 06:38:45 +0000

    Draft slides for atomics and some code to SVN
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@314 39143b06-f351-456c-9f92-4cd32fad6823

---
+++ cpp_code/threads/07-dining-oops.cc
@@ -0,0 +1,78 @@
+// dining philosophers and deadlock avoidnance
+
+#include <chrono>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::lock_guard;
+using std::mutex;
+using std::thread;
+using std::vector;
+
+mutex mstdout;
+
+constexpr int numdiners = 6;
+constexpr int NPORTIONS = 10;
+constexpr int EATTIME = 10;
+constexpr int THINKTIME = 10;
+constexpr int TAKETIME = 1;
+
+void mylock(mutex &left, mutex &right) {
+  left.lock();
+  // std::this_thread::sleep_for(std::chrono::milliseconds(TAKETIME));
+  right.lock();
+}
+
+class Philosopher {
+  int num_;
+  mutex &left_, &right_;
+
+public:
+  Philosopher(int num, mutex &left, mutex &right) : 
+    num_(num), left_(left), right_(right) {
+  }
+
+  void msgout() const {  
+    lock_guard<mutex> lk{mstdout};
+    cout << num_ << " eating" << endl;
+  }
+
+  void eat() {
+    // take forks from left and right
+    mylock(left_, right_);
+
+    // at this point forks are taken
+    msgout();    
+    std::this_thread::sleep_for(std::chrono::milliseconds(EATTIME));
+    left_.unlock();
+    right_.unlock();
+  }
+
+  void dine() {
+    for (int i = 1; i < NPORTIONS; ++i) {
+      eat();
+      std::this_thread::sleep_for(std::chrono::milliseconds(THINKTIME));
+    }
+  }
+};
+
+int
+main () {
+  vector<mutex> forks(numdiners);
+  vector<Philosopher> diners;
+  for (int i = 0; i < numdiners; ++i) {
+    int j = (i == (numdiners - 1)) ? 0 : i + 1;
+    diners.emplace_back(i + 1, forks[i], forks[j]);
+  }
+
+  vector<thread> threads(numdiners);
+  for (int i = 0; i < numdiners; ++i) {
+    threads[i] = thread(&Philosopher::dine, diners[i]);
+  }
+  for (auto& t : threads)
+    t.join();
+}
---
+++ cpp_code/threads/12a-classical-prod-cons.cc
@@ -0,0 +1,91 @@
+// Classical producer-consumer with fixed buffer
+
+#include <condition_variable>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <queue>
+#include <thread>
+#include <utility>
+#include <vector>
+
+using std::chrono::duration_cast;
+using std::chrono::high_resolution_clock;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+using std::condition_variable;
+using std::cout;
+using std::endl;
+using std::lock_guard;
+using std::make_unique;
+using std::move;
+using std::mutex;
+using std::queue;
+using std::this_thread::sleep_for;
+using std::thread;
+using std::unique_lock;
+using std::unique_ptr;
+using std::vector;
+
+constexpr int BUFSIZE = 10;
+constexpr int MAXTASKS = 10000;
+
+vector<int> buffer(BUFSIZE);
+int ncur = 0;
+int nsolved = 0;
+mutex mut;
+condition_variable data_cond_c, data_cond_p;
+
+void producer() {
+  for(;;) {
+    // critical section
+    {
+      unique_lock<mutex> lk{mut};
+      while (ncur == BUFSIZE) 
+        data_cond_p.wait(lk);        
+      buffer[ncur] = nsolved;
+      ncur += 1;
+#ifdef SHOW
+      cout << "+";
+#endif
+    }
+    data_cond_c.notify_one();
+  }
+}
+
+void consumer() {
+  for(;;) {
+    // critical section
+    {
+      unique_lock<mutex> lk{mut};
+      while (ncur == 0)
+        data_cond_c.wait(lk);
+      nsolved += 1;
+      ncur -= 1;
+#ifdef SHOW
+      cout << "-";
+#endif
+      if (nsolved > MAXTASKS)
+        exit(0);
+    }
+    data_cond_p.notify_one();
+  }
+}
+
+int main() {
+  thread p{producer}; 
+  thread c{consumer};
+
+#ifdef SECOND
+  thread p2{producer};
+  thread c2{consumer};
+#endif
+  
+  p.join();
+  c.join(); 
+
+#ifdef SECOND
+  p2.join();
+  c2.join();
+#endif
+}
