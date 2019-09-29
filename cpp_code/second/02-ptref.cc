228b7e9f25af629ef8e96e1207ea520df3e00282
tilir <konstantin.vladimirov@gmail.com>
2019-09-29 12:09:43 +0000

    Adding more code samples and slides, some correction to main text
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@352 39143b06-f351-456c-9f92-4cd32fad6823

---
+++ cpp_code/second/02-ptref.cc
@@ -0,0 +1,9 @@
+int g;
+
+void foo(int *x) {
+  g = *x;
+}
+
+void bar(int &x) {
+  g = x;
+}
---
+++ cpp_code/second/02-this.cc
@@ -0,0 +1,15 @@
+struct S {
+  S *retme() {
+    return this;
+  }
+};
+
+S * retfirst(S *ps) {
+  return ps;
+}
+
+S g;
+
+S *foo() {
+  return g.retme();
+}
