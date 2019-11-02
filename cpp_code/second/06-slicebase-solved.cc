a57c0503907827bd39ba7c23c51bad46503af216
tilir <konstantin.vladimirov@gmail.com>
2019-11-02 11:46:35 +0000

    Code and slides for inheritance seminar
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@362 39143b06-f351-456c-9f92-4cd32fad6823

--- cpp_code/second/06-slicebase-solved.cc
+++ cpp_code/second/06-slicebase-solved.cc
@@ -3,11 +3,22 @@
 struct A {
   int a_;
   A(int a) : a_(a) {}
+  virtual A &operator=(const A &rhs) {
+    a_ = rhs.a_;
+    return *this;
+  }
+  virtual ~A() {}
 };
 
 struct B : public A {
   int b_;
   B(int b) : A(b / 2), b_(b) {}
+  B &operator=(const A &rhs) override {
+    const B &brhs = static_cast<const B &>(rhs);
+    a_ = brhs.a_;
+    b_ = brhs.b_;
+    return *this;
+  }
 };
 
 std::ostream &operator<<(std::ostream &os, const B &b) {
