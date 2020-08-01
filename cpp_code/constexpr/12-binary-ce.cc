2367e767240704fda1d52364b419ab9c9ae86e7b
tilir <konstantin.vladimirov@gmail.com>
2020-08-01 11:21:33 +0000

    Slides and amended example
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@409 39143b06-f351-456c-9f92-4cd32fad6823

--- cpp_code/constexpr/12-binary-ce.cc
+++ cpp_code/constexpr/12-binary-ce.cc
@@ -1,18 +1,20 @@
+#include <array>
 #include <iostream>
 
 using std::cout;
 using std::endl;
 
 template<typename ... Ts>
-constexpr unsigned long long binparser (unsigned long long accum, char c, Ts ... cs) {
-  unsigned digit = (c == '1') ? 1 : (c == '0') ? 0 : throw "out of range"; 
-  accum = accum * 2 + digit;
-  if constexpr (sizeof...(Ts) != 0)
-    return binparser(accum, cs...);
+constexpr unsigned long long binparser (unsigned long long accum, Ts ... cs) {  
+  std::array tmp{cs...};
+  for (auto&& c : tmp) {
+    unsigned digit = (c == '1') ? 1 : (c == '0') ? 0 : throw "out of range"; 
+    accum = accum * 2 + digit;
+  }
   return accum;
 }
 
-template<char ... Chars>
+template<char ... Chars> // '1', '0', '1', .... , '1'
 constexpr unsigned long long operator "" _binary() {
   return binparser (0ull, Chars...); 
 }
