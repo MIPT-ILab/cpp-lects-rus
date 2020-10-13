a437f20dbd866a9d0d153f3cade7899e7735597c
tilir <konstantin.vladimirov@gmail.com>
2020-10-13 11:25:25 +0000

    Fixing some slides
    git-svn-id: https://svn.code.sf.net/p/cpp-lects-rus/code/trunk@421 39143b06-f351-456c-9f92-4cd32fad6823

--- other_lects/informatics/code/02-resources/algs.c
+++ other_lects/informatics/code/02-resources/algs.c
@@ -8,21 +8,38 @@ struct sieve_t {
   unsigned char *sieve;
 };
 
+// 1 если составное
+int is_composite(struct sieve_t s, int i) {
+  int nbyte = i / 8;
+  int nbit = i % 8;
+  return (s.sieve[nbyte] >> nbit) & 1;
+}
+
+void set_composite(struct sieve_t s, int i) {
+  int nbyte = i / 8;
+  int nbit = i % 8;
+  s.sieve[nbyte] = s.sieve[nbyte] | (1u << nbit);
+}
+
 struct sieve_t 
 init_sieve (unsigned n) 
 {
+  struct sieve_t res;
   int i, j;
+  unsigned char *sieve;
   assert(n > 2);
-  unsigned char *sieve = calloc(n, sizeof(unsigned char));
-  struct sieve_t res = { n, sieve };
+  sieve = calloc(n, sizeof(unsigned char));
+  res.size = n;
+  res.sieve = sieve;
   assert ((n > 1) && (sieve != NULL));
   unsigned r = (unsigned) sqrt (n) + 1;
-  res.sieve[0] = res.sieve[1] = 1;
-
-  for (i = 2; i * i <= n; ++i) {
-    if (res.sieve[i] != 0)
+  res.sieve[0] = 0x3;
+  for (i = 2; i < r; ++i) {
+    // если число составное
+    if (is_composite(res_sieve, i)) 
       continue;
-    for (j = i * i; j <= n; j += i) 
+    for (j = i * i; j < n; j += i)
+      set_composite(res_sieve, i);
   }
 
   return res;
